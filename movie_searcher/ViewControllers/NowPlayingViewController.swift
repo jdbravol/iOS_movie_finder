207da96ea25d35674eb441be126940b79af355f0
jdbravol <jdbravol@utexas.edu>
2018-02-26 23:04:35 -0600

    autoLayout implemented

--- movie_searcher/ViewControllers/NowPlayingViewController.swift
+++ movie_searcher/ViewControllers/NowPlayingViewController.swift
@@ -26,7 +26,8 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         
         tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
-        tableView.rowHeight = 190
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 50
         fetchMovies()
     
     }
